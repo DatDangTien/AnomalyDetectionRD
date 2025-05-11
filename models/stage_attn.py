9f3c3d74051cfd3db019bd0f134e01a96da850c8
DatDangTien <116250191+DatDangTien@users.noreply.github.com>
2025-05-11 19:11:49 +0700

    debug: Layer attn save miss param.

--- models/stage_attn.py
+++ models/stage_attn.py
@@ -73,9 +73,9 @@ class AdaptiveStagesFusion(nn.Module):
                 nn.Linear(feat.shape[1], 1, device=self.device)
             ) for feat in x
         ])
-        print(self.linears)
+        # print(self.linears)
         self.add_module('linears', self.linears)
-        print(self)
+        # print(self)
 
         for block in self.linears:
             for layer in block:
