ab608da1a5b82315a11b425473f5811ebc68bcfb
DatDangTien <116250191+DatDangTien@users.noreply.github.com>
2025-05-12 21:08:14 +0700

    debug: loss component.

--- models/stage_attn.py
+++ models/stage_attn.py
@@ -53,15 +53,12 @@ class AdaptiveStagesFusion(nn.Module):
         # Normalize
         if self.trainable:
             w = (w * fusion_scores)
-        # print('w: ',w)
         w = w.softmax(dim=0)
         # Scale
         if self.scale:
             w = w * self.num_stages
 
         print('true w', self.weight)
-        print('fusion', fusion_scores)
-        print('w output:', w)
         return w
 
     def _init_linears(self, x):
@@ -114,15 +111,11 @@ def adap_loss_function(a, b, w_module=None,
     else:
         w = w_module(b)
 
-    # print('w', w)
-
-    print('w: ', w)
     loss = torch.tensor(0.0, device=device)
     for item in range(len(a)):
         stage_loss = torch.mean(1 - cos_loss(a[item].view(a[item].shape[0], -1),
                                              b[item].view(b[item].shape[0], -1)))
         loss = loss + w[item] * stage_loss
-        print('stage', stage_loss)
 
     # Entropy penalty
     gini = 1 - torch.sum((w / len(w)) ** 2)
@@ -130,9 +123,6 @@ def adap_loss_function(a, b, w_module=None,
 
     # Weight loss with entropy
     loss = loss + w_entropy * penalty
-    print('loss: ',loss)
-    print('penalty: ',w_entropy * penalty)
-    # print(loss)
     return loss
 
 
@@ -141,9 +131,6 @@ def cal_anomaly_map(a,b, w_module=None, out_size=224, amap_mode='mul'):
         w = torch.ones(len(a))
     else:
         w = w_module(b)
-
-    # print('w', w)
-
 
     if amap_mode == 'mul':
         anomaly_map = np.ones([out_size, out_size])
