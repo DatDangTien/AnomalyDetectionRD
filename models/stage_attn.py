d92f98c1fc8e73adfa3f1e5dd0f10032c569774b
DatDangTien <116250191+DatDangTien@users.noreply.github.com>
2025-05-13 12:25:22 +0700

    debug: loss penalty

--- models/stage_attn.py
+++ models/stage_attn.py
@@ -123,7 +123,9 @@ def adap_loss_function(a, b, w_module=None,
 
     # Weight loss with entropy
     loss = loss + w_entropy * penalty
-    print(w_entropy * penalty)
+    if loss > 1:
+        print('High penalty: ', w_entropy * penalty)
+
     return loss
 
 
